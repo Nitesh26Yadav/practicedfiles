ae5b21030ddfb84c042c00cc30c9e30e4aea9391
Nitesh26Yadav <nitesh.156007@gmail.com>
2023-01-03 19:14:01 +0530

    practice

--- djangopractice/project1/food/models.py
+++ djangopractice/project1/food/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+class Item(models.Model):
+
+    def __str__(self):
+        return self.item_name
+
+    item_name = models.CharField(max_length=200)
+    item_desc = models.CharField(max_length=200)
+    item_price = models.IntegerField()
+
+
+class favoriteItem(models.Model):
+
+    def __str__(self):
+        return self.favorite_item_name
+
+    favorite_item_name = models.CharField(max_length=200)
+    favorite_item_desc = models.CharField(max_length=200)
+    favorite_item_price = models.IntegerField()
+    favorite_item_quantity = models.IntegerField()
--- djangopractice/project1/food/views.py
+++ djangopractice/project1/food/views.py
@@ -5,3 +5,8 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse(" Hello World ")
     
+def item(request):
+    return HttpResponse('<h1>This is the Item view</h1>')
+
+def favoriteitems(request):
+    return HttpResponse('This is your favorites Items.')
