796ed3fbe078df737439f5f85ea0b7de4c6462df
Nitesh26Yadav <nitesh.156007@gmail.com>
2023-02-07 17:42:24 +0530

    feb 7

--- djangopractice/project1/food/views.py
+++ djangopractice/project1/food/views.py
@@ -9,10 +9,21 @@ def index(request):
     item_list = Item.objects.all()
     template = loader.get_template('food/index.html')
     context = {
-
+        'item_list': item_list
     }
-    return HttpResponse(template.render(context, request))
+    # return HttpResponse(template.render(context, request))
+    return render(request, 'food/index.html', context)
 
 
 def item(request):
     return HttpResponse("<h1>Item View</h1>")
+
+
+def detail(request, item_id):
+    item = Item.objects.get(pk=item_id)
+    context = {
+        'item': item
+    }
+    # return ("This is item id no: %s" % item_id)
+    # return HttpResponse(f"This is item Id no: {item_id}")
+    return render(request, 'food/detail.html', context)
