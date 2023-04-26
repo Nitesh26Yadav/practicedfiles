3b025e5d9f1d190ad3c4892bf16abcabeb5275b7
Nitesh26Yadav <nitesh.156007@gmail.com>
2023-04-26 22:41:40 +0530

    base template

--- djangopractice/project1/food/models.py
+++ djangopractice/project1/food/models.py
@@ -11,3 +11,5 @@ class Item(models.Model):
     item_name = models.CharField(max_length=200)
     item_desc = models.CharField(max_length=200)
     item_price = models.IntegerField()
+    item_image = models.CharField(
+        max_length=500, default="https://th.bing.com/th/id/R.861d624cb303db96322dcaeb6ed99e9b?rik=CstlEO2%2fm6Xy4w&riu=http%3a%2f%2fthefooddisciple.com%2fwp-content%2fuploads%2f2015%2f09%2fneptune-placeholder-40.jpg&ehk=oOphLDO0j3MFugqqnhbpZY5C1OyvfNcEvqRdyVpkIzk%3d&risl=&pid=ImgRaw&r=0")
--- djangopractice/project1/project1/settings.py
+++ djangopractice/project1/project1/settings.py
@@ -64,7 +64,11 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+            'libraries': {
+                'staticfiles': 'django.templatetags.static',
+            },
         },
+
     },
 ]
 
