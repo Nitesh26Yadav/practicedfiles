a537078fd5314d61c6655186992929dfeab275e6
Nitesh26Yadav <nitesh.156007@gmail.com>
2023-05-01 22:44:02 +0530

    adding forms- add,delete,edit,update

--- djangopractice/project1/food/urls.py
+++ djangopractice/project1/food/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from . import views
 
-
 app_name = "food"
 urlpatterns = [
     # food
@@ -9,4 +8,10 @@ urlpatterns = [
     # food/1
     path('<int:item_id>/', views.detail, name='detail'),
     path('item/', views.item, name='item'),
+    # add item
+    path('add/', views.create_item, name='create_item'),
+    # edit item
+    path('update/<int:item_id>/', views.update_item, name='update_item'),
+    # delete item
+    path('delete/<int:item_id>/', views.delete_item, name='delete_item'),
 ]
--- djangopractice/project1/food/views.py
+++ djangopractice/project1/food/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, request
 from . models import Item
 from django.template import loader
+from . forms import ItemForm
 # Create your views here.
 
 
@@ -27,3 +28,34 @@ def detail(request, item_id):
     # return ("This is item id no: %s" % item_id)
     # return HttpResponse(f"This is item Id no: {item_id}")
     return render(request, 'food/detail.html', context)
+
+
+def create_item(request):
+    form = ItemForm(request.POST or None)
+
+    if form.is_valid():
+        form.save()
+        return redirect('food:index')
+
+    return render(request, 'food/item-form.html', {'form': form})
+
+
+def update_item(request, item_id):
+    item = Item.objects.get(pk=item_id)
+    form = ItemForm(request.POST or None, instance=item)
+
+    if form.is_valid():
+        form.save()
+        return redirect('food:index')
+
+    return render(request, 'food/item-form.html', {'form': form, 'item': item})
+
+
+def delete_item(request, item_id):
+    item = Item.objects.get(pk=item_id)
+
+    if request.method == 'POST':
+        item.delete()
+        return redirect('food:index')
+
+    return render(request, 'food/item-delete.html', {'item': item})
